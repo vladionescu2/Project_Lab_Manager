1844092c8605aa201d1f691460bc9efea668bc5d
Vlad-George Ionescu <vlad-george.ionescu@student.manchester.ac.uk>
2021-03-18 20:16:03 +0200

    Improve mock data and add end time for lab

--- src/main/java/project/lab_management_syst/persistence/model/CourseUnit.java
+++ src/main/java/project/lab_management_syst/persistence/model/CourseUnit.java
@@ -3,9 +3,7 @@ package project.lab_management_syst.persistence.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,9 +11,22 @@ import java.util.List;
 @Data
 @EqualsAndHashCode
 public class CourseUnit {
+
+    @Entity
+    @Data
+    @EqualsAndHashCode
+    static
+    class LabTimes {
+        @Id
+        @GeneratedValue
+        private Long id;
+        LocalDateTime start;
+        LocalDateTime end;
+    }
+
     @Id
     String unitCode;
 
-    @ElementCollection
-    List<LocalDateTime> labTimes;
+    @OneToMany
+    List<LabTimes> labTimes;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,73 @@
 INSERT INTO COURSE_UNIT(UNIT_CODE) VALUES ('COMP0'), ('COMP1'), ('COMP2');
-INSERT INTO COURSE_UNIT_LAB_TIMES(COURSE_UNIT_UNIT_CODE, LAB_TIMES) VALUES
-    ('COMP0', '20210311 12:00:00'),
-    ('COMP0', '20210318 12:00:00'),
-    ('COMP0', '20210325 12:00:00'),
-    ('COMP0', '20210401 10:00:00'),
-    ('COMP1', '20210310 10:00:00'),
-    ('COMP1', '20210317 10:00:00'),
-    ('COMP1', '20210324 10:00:00'),
-    ('COMP2', '20210315 14:00:00'),
-    ('COMP2', '20210316 15:00:00'),
-    ('COMP2', '20210322 14:00:00'),
-    ('COMP2', '20210323 15:00:00');
+
+INSERT INTO COURSE_UNIT$LAB_TIMES(ID, START, END) VALUES
+    (
+        0,
+        DATEADD('ww', -1, DATEADD('hh', 14, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today))),
+        DATEADD('ww', -1, DATEADD('hh', 16, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today)))
+    ),
+    (
+        1,
+        DATEADD('ww', 0, DATEADD('hh', 14, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today))),
+        DATEADD('ww', 0, DATEADD('hh', 16, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today)))
+    ),
+    (
+        2,
+        DATEADD('ww', 1, DATEADD('hh', 14, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today))),
+        DATEADD('ww', 1, DATEADD('hh', 16, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today)))
+    ),
+    (
+        3,
+        DATEADD('ww', -1, DATEADD('hh', 10, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))),
+        DATEADD('ww', -1, DATEADD('hh', 12, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today)))
+    ),
+    (
+        4,
+        DATEADD('ww', 0, DATEADD('hh', 10, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))),
+        DATEADD('ww', 0, DATEADD('hh', 12, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today)))
+    ),
+    (
+        5,
+        DATEADD('ww', 1, DATEADD('hh', 10, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))),
+        DATEADD('ww', 1, DATEADD('hh', 12, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today)))
+    ),
+    (
+        6,
+        DATEADD('ww', -1, DATEADD('hh', 15, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today))),
+        DATEADD('ww', -1, DATEADD('hh', 16, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today)))
+    ),
+    (
+        7,
+        DATEADD('ww', 0, DATEADD('hh', 15, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today))),
+        DATEADD('ww', 0, DATEADD('hh', 16, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today)))
+    ),
+    (
+        8,
+        DATEADD('ww', 1, DATEADD('hh', 15, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today))),
+        DATEADD('ww', 1, DATEADD('hh', 16, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today)))
+    );
+
+INSERT INTO COURSE_UNIT_LAB_TIMES(COURSE_UNIT_UNIT_CODE, LAB_TIMES_ID) VALUES
+    ('COMP0', 0),
+    ('COMP0', 1),
+    ('COMP0', 2),
+    ('COMP1', 3),
+    ('COMP1', 4),
+    ('COMP1', 5),
+    ('COMP2', 6),
+    ('COMP2', 7),
+    ('COMP2', 8);
 
 INSERT INTO LAB_FORMAT(REPO_NAMING_SCHEMA, COURSE_UNIT_UNIT_CODE) VALUES
     ('COMP0_Lab', 'COMP0'), ('COMP1_Lab', 'COMP1'), ('COMP2_Lab', 'COMP2');
+
 INSERT INTO LAB_FORMAT_LAB_EXERCISES(LAB_FORMAT_REPO_NAMING_SCHEMA, LAB_EXERCISES, LAB_EXERCISES_KEY) VALUES
-    ('COMP0_Lab', '20220619 14:00:00', 'ex_1'),
-    ('COMP0_Lab', '20220812 14:00:00', 'ex_2'),
-    ('COMP0_Lab', '20221002 14:00:00', 'ex_3'),
-    ('COMP1_Lab', '20220319 14:00:00', 'ex_1'),
-    ('COMP1_Lab', '20220520 14:00:00', 'ex_2'),
-    ('COMP1_Lab', '20220920 14:00:00', 'ex_3'),
-    ('COMP2_Lab', '20220604 14:00:00', 'ex_1'),
-    ('COMP2_Lab', '20220801 14:00:00', 'ex_2'),
-    ('COMP2_Lab', '20201022 14:00:00', 'ex_3');
+    ('COMP0_Lab', DATEADD('ww', -1, DATEADD('hh', 18, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 4, today))), 'ex_1'),
+    ('COMP0_Lab', DATEADD('ww', 2, DATEADD('hh', 18, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 4, today))), 'ex_2'),
+    ('COMP0_Lab', DATEADD('ww', 4, DATEADD('hh', 18, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 4, today))), 'ex_3'),
+    ('COMP1_Lab', DATEADD('ww', 3, DATEADD('hh', 14, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))), 'ex_1'),
+    ('COMP1_Lab', DATEADD('ww', 5, DATEADD('hh', 14, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 3, today))), 'ex_2'),
+    ('COMP1_Lab', DATEADD('ww', 9, DATEADD('hh', 14, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7), today))), 'ex_3'),
+    ('COMP2_Lab', DATEADD('ww', 4, DATEADD('hh', 12, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))), 'ex_1'),
+    ('COMP2_Lab', DATEADD('ww', 5, DATEADD('hh', 12, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))), 'ex_2'),
+    ('COMP2_Lab', DATEADD('ww', 6, DATEADD('hh', 12, DATEADD('dd', -((DAY_OF_WEEK(today) - 2) % 7) + 2, today))), 'ex_3');
